9a5d0336c71ef62440fa414e8b06f3c31a6d3b2a
Alikhalid2020 <lanrakhaled@gmail.com>
2021-01-19 21:22:50 +0300

    added the overlay function in js

--- instagram/static/js/scripts.js
+++ instagram/static/js/scripts.js
@@ -1,7 +1,7 @@
 
   $(document).ready(function(){
     $(".im").mouseover(function(){
-      $("#overlay").show();
+      $("#overla").show();
     }).mouseout(function(){
       $("#overlay").hide();
     });
